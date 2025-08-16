0ab0edd3a9e434f234a92f3a799c52c5296f0e67
Davido <davidepinzan3@gmail.com>
2025-08-16 19:21:58 +0200

    Added OAuth metadata

--- server.js
+++ server.js
@@ -1,21 +1,38 @@
 const express = require('express')
 const app = express()
+const baseUrl = "https://server"
+
+app.get("/.well-known/openid-configuration", (req, res) => {
+  const metadata = {
+    issuer: `${baseUrl}`,
+    token_endpoint: `${baseUrl}/token`,
+    response_types_supported: ["token"]
+  }
+  res.json(metadata)
+})
 
 
-app.get('/.wellxknown/openid-credential-issuer', (req, res) => {
-  var metadata = {
-    credential_issuer: "https://test", //TODO when hosting
-    credential_endpoint: "https://test/credential",
+app.post("/credential", (req, res) => {})
+
+app.get('/.well-known/openid-credential-issuer', (req, res) => {
+  const metadata = {
+    credential_issuer: `${baseUrl}`, //TODO when hosting
     credential_configurations_supported: {
       "Visa": {
         format: "jwt_vc_json"
       },
+
       "UniversityDegree": {
         format: "jwt_vc_json"
       }
-    }
+    },
+    credential_endpoint: `${baseUrl}/credential`,
   }
+  res.json(metadata)
 })
+
+
+app.post("/token", (req, res) => {})
 
 app.listen(3000, () => {
   console.log(`Listening on: ${port}`)
