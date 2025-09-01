ab73b9e549b15ce110a44f32c00e33fef2e83a2e
Davido <davidepinzan3@gmail.com>
2025-09-01 20:31:12 +0200

    Added token signature

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express')
+const jose = require('jose')
 const app = express()
+
 const baseUrl = "http://10.0.2.2:3000"
 
 var sessionIds = []
 
 const ENC = 'base64'
+const config = require('./config.json')
+const private = jose.importJWK(config.jwk, 'Ed25519')
+const public = jose.importJWK(config.public, 'Ed25519')
 
 app.use(express.urlencoded());
 
@@ -20,35 +25,25 @@ app.get("/.well-known/openid-configuration", (req, res) => {
 
 app.post("/credential", (req, res) => {})
 
-app.post("/token", (req, res) => {
+app.post("/token", async (req, res) => {
 
-  var preauth = req.body['pre-authorized_code']
-  var payload = JSON.parse(Buffer.from(preauth.split('.')[1], ENC).toString());
+  let preauth = req.body['pre-authorized_code']
+  let {payload, protectedHeader} = await jose.jwtVerify(preauth, await public)
 
   console.log(preauth)
 
   if ((payload.aud === "TOKEN" || payload.iss === baseUrl) && sessionIds.includes(payload.sub)) {
     payload.aud = "ACCESS"
-    let accessBearerToken = preauth.split('.')[0] + '.' + Buffer.from(JSON.stringify(payload)).toString(ENC) + '.' + preauth.split('.')[2] //placeholder
-
-    
+    //let accessBearerToken = preauth.split('.')[0] + '.' + Buffer.from(JSON.stringify(payload)).toString(ENC) + '.' + preauth.split('.')[2] //placeholder
+    let accessBearerToken = await new jose.SignJWT(payload).setProtectedHeader(protectedHeader).sign(private)
     let index = sessionIds.indexOf(payload.sub)
     sessionIds.splice(index, 1)
     
     res.json({access_token: accessBearerToken, token_type: "Bearer"})
 
-
-
-
-
-
-
-
-
-
-
-
   }
+
+  
 })
 
 
